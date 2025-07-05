15920e43f3197766c936fd872ee5d6926dd64579
ate32 <ate32.champ@gmail.com>
2025-07-06 01:52:57 +0530

    02-Igniting our App

--- App.js
+++ App.js
@@ -1,3 +1,7 @@
+
+import React from  "react"
+import ReactDOM from "react-dom/client"
+
 let heading= React.createElement("h1", {id:'heading', xyz:"ABC"}, 'Hello Aniket');
 const root= ReactDOM.createRoot(document.getElementById('root'));
 root.render(heading);
