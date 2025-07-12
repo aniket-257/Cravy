d9848f399ff616f2f18575274d9ef628e50b2704
ate32 <ate32.champ@gmail.com>
2025-07-13 01:59:38 +0530

    04 - Talk is cheap, Show me the code !

--- App.js
+++ App.js
@@ -1,49 +1,88 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+// import LogoImg from "./assests/Cravy"; // ! Why Not working ?
 
-// React.createElement =>React Element (JS Object) => Create HTML Element using Render method
-let heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello Aniket using core React 🚀"
-);
+/**
+ * Header
+ *  - Logo
+ *  - Nav Bar
+ * Body
+ *  - Search Bar
+ *  - RestaurantContainer
+ *      - RestroCard
+ * Footer
+ *  - Copyright
+ *  - Links
+ *  - Contacts
+ */
 
-// * JSX is a XML or HTML like struture inside JS
-// * JSX is not a HTML inside JS
-// * JSX also return a React Element Object
-// * JSX is not a React
-// * heading and jsxHeading objects are same
-// * JSX is not a pure JS Syntax -> JS Engines can't understand JSX code
-// * JSX (Transcompiled before it reaches to JS Engine) => PARCEL (BABEL) task
-// * JSX => Babel transpiled it to React.createElement => React Element (JS Object) => Create HTML Element using Render Method
-// * JSX attributes are in camelCase (in HTML we have tabindex but in JSX we write tabIndex)
-
-// React Functional Component => Normal JS Which return JSX code
-let Heading = () => {
-  return <h3>I am Heading Component 🚀</h3>;
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://raw.githubusercontent.com/aniket-257/React/refs/heads/main/assets/CravyTransparent.png"
+          alt="Cravy Logo"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
-// React Element => JSX code
-let jsxHeading = (
-  <div id="heading">
-    <Heading /> Hello Aniket using JSX 🚀
-  </div>
-);
+const RestaurantCard = () => {
+  return (
+    <div className="res-card">
+      <img
+        className="res-img"
+        alt="res-img"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2025/6/9/d37a7eaf-9044-4187-a761-87bbec10f1e6_14781.JPG"
+      />
+      <h3 className="res-name">Pizza Hutttttttttttttttttttttttttttt</h3>
+      <h3 className="res-details">4.2 Stars - 30 mins</h3>
+      <h3 className="res-cuisines">
+        Pizzas, Biryani, north indian, chinese Pizzas, Biryani, north indian,
+        chinese
+      </h3>
+      <h3 className="res-details">Wakad</h3>
+    </div>
+  );
+};
 
-// Component Composition => Component inside Component
-// {JS inside curly braces}
-let Title = () => (
-  <div className="title">
-    <h2>{10 * 200}</h2>
-    <h2>{console.log("Javascript inside JSX")}</h2>
-    <h1>I am Title Component ✨</h1>
-    {Heading()}
-    <Heading />
-    <Heading></Heading>
-    {jsxHeading}
-  </div>
-);
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(heading); // React Element Rendering Syntax
-root.render(<Title />); // React Component Rendering Syntax
+root.render(<AppLayout />); // React Component Rendering Syntax
