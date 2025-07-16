ff6675594430634c80123d563187414695d7e884
ate32 <ate32.champ@gmail.com>
2025-07-16 23:32:10 +0530

    React useState Hook example Added

--- README.md
+++ README.md
@@ -32,3 +32,21 @@
 ## Default Import
 
     import component from ...
+
+## React Hooks
+
+# useState() => 70% Usecase
+
+# UseEffect() => 20% Usecase
+
+# other Hooks => 10% USecase
+
+- React Hooks are nothing but a Normal Javascript utility functiuons provided by React
+- They are use to defined Super powerfull Variables
+- useState return an array of variable name and setVariable Function
+- setVaribale function is use to change value of variable (variable changes using normal assignment will not take into consideration)
+
+# Reconsiliation (React Fiber)
+
+- it uses a virtual DOM to do efficient DOM manipulation
+- Virtual DOM nothing but a Javascript Object (Object(Virtual DOM) comparison is faster than comparing two Actual DOM)
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,23 @@
 import RestaurantCard from "./RestaurantCard";
-import restaurants from "../utils/mockData";
+import allResList from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+  const [restaurants, setRestaurants] = useState(allResList);
+
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <button
+        className="top-restaurants"
+        onClick={() => {
+          let filteredRes = allResList.filter(
+            (res) => res.info.avgRating >= 4.5
+          );
+          setRestaurants(filteredRes);
+        }}
+      >
+        Filter Top Restaurant{" "}
+      </button>
       <div className="res-container">
         {restaurants.map((res) => (
           <RestaurantCard resObj={res} key={res?.info?.id} />
