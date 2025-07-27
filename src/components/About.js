f8e73c2a1d138364ac3a177d3e5727bbaeccc1d6
ate32 <ate32.champ@gmail.com>
2025-07-27 15:46:53 +0530

    Added Extra Class based Component Notes and concepts

--- src/components/About.js
+++ src/components/About.js
@@ -3,11 +3,15 @@ import UserClass from "./UserClass";
 import React from "react";
 
 class About extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      parentCount: 0,
-    };
+  constructor() {
+    //! Always call super() first in constructors of class components.
+    //! Even if you're not using this, React might internally.
+    //! else don't write construtor if we have constructor then compulsory write
+    // super()
+    //? When?	When a class extends another using extends
+    //?Why?	To correctly initialize this and inherit from the parent
+    //! What if skipped?	Throws ReferenceError: Must call super constructor in derived class before accessing 'this'
+    super();
     console.log("Parent Constructor");
   }
 
@@ -19,59 +23,12 @@ class About extends React.Component {
     console.log("Parent Render");
     return (
       <div className="about">
-        <h1>Parent Count: {this.state.parentCount}</h1>
-        <button
-          onClick={() => {
-            this.setState({
-              parentCount: this.state.parentCount + 1,
-            });
-          }}
-        >
-          Increase Parent Count
-        </button>
         <h1>About Page</h1>
         <h2>Building React Project 🚀</h2>
         <UserClass name="Aniket" location="Pune" contact="@ate32" />
-        <UserClass name="Shrutika" location="Nashik" contact="@keshu3012" />
       </div>
     );
   }
 }
-
-//
-// Parent Constructor
-// Parent Render
-//? React batche the phases for optimazation because Commit phase is expensive
-//! Batch Render Phase (Construtor + Render) (Javascript Part of React so it is fast) (Calculating Diff for virtual DOM)
-//     Aniket Constructor
-//     Aniket Render
-//     Shrutika Constructor
-//     Shrutika Render
-//!✅ At this point, the virtual DOM is fully built, and React commits it to the real DOM.
-//! Batch Commit Phase (DOM Updates + ComponentDidMount) (Actual DOM manipulation so it is slow)
-//! <DOM UPDATED IN SINGLE BATCH GOOOO/>
-//     Aniket Component Did Mount?
-//     Shrutika Component Did Mount?
-// Parent Component Did Mount?
-//
-
-// const About = () => {
-//   return (
-//     <div className="about">
-//       <h1>About Page</h1>
-//       <h2>Building React Project 🚀</h2>
-//       <User
-//         name="Aniket Tarale (Functional Component)"
-//         location="Pune"
-//         contact="@ate32"
-//       />
-//       <UserClass
-//         name="Shrutika Donde (Class Based Components)"
-//         location="Nashik"
-//         contact="@keshu3012"
-//       />
-//     </div>
-//   );
-// };
 
 export default About;
--- src/components/UserClass.jsx
+++ src/components/UserClass.jsx
@@ -4,40 +4,83 @@ class UserClass extends React.Component {
     super(props);
     // this.state object is reserve for state variables
     this.state = {
-      count: 0,
+      userInfo: {
+        name: "Dummy",
+        location: "Dummy Location",
+      },
     };
     console.log(this.props.name + " Constructor");
   }
 
-  componentDidMount() {
+  async componentDidMount() {
+    // this.timer = setInterval(() => {
+    //   console.log("..............React OP.............");
+    // this will run always (because react doesn't refresh browser)
+    // need to clear inside componentWillUnmount
+    // }, 1000);
     console.log(this.props.name + " Component Did Mount?");
     //API Call after render method
+    //componentDidMount is not useEffect in functional component
+    //we can say useEffect with [empty Dependency] is work similar to componentDidMount because it will call once after component render
+    const data = await fetch(" https://api.github.com/users/anni-257");
+    const json = await data.json();
+    this.setState({
+      userInfo: json,
+    });
+    console.log(json);
+
+    // it will wait to get data from API meanwhile parent componentDidMount method will execute
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log("Child component Did Update?");
+    // if(this.state.count !== prevState.count || this.state.count2 !== prevState.count2){
+    // work similar to useEffect with [count, count2] dependencies
+    // }
+    // if(this.state.count3 !== prevState.count3){
+    // work similar to another useEffect with [count3] dependency
+    // }
+    //! It was a Pain to write if else condition in componentDidUpdate
+  }
+
+  componentWillUnmount() {
+    console.log("Child component Will Unmount?");
+    // clear our mess like setIntervals, settimeout etc
+    // this will run in the background because our react is a SPA(Single Page Application)
+    // explicitly we have to clear setIntervals
+    clearInterval(this.timer);
   }
   render() {
     console.log(this.props.name + " Render");
-    const { name, location, contact } = this.props;
+    const { name, location, avatar_url } = this.state.userInfo;
     return (
       <div className="user">
-        <h1>{this.state.count}</h1>
-        <button
-          onClick={() => {
-            //! Never update state variable like this
-            //? this.state.count = this.state.count + 1; //! this will not work
-            //! this.setState({stateVariables:updatedValues}) is used to update value on UI, only given states will update and another will be untouched
-            this.setState({
-              count: this.state.count + 1,
-              //count2: this.state.count2+1
-            });
-          }}
-        >
-          Count Increase
-        </button>
+        <img src={avatar_url} />
         <h3>Dev Name: {name}</h3>
         <h3>Location: {location}</h3>
-        <h3>Contact:{contact}</h3>
+        {/* <h3>Contact:{contact}</h3> */}
       </div>
     );
   }
 }
 
 export default UserClass;
+
+//? ----Mounting----
+//!Render Phase
+// constructor (Dummy)
+// render (Dummy)
+
+//!Commit Phase
+//<HTML Updated (Dummy)/>
+//componentDidMount
+// <API CALL/>
+// <setState/> update state variable
+// triggers update cycle
+
+//? ----Update----
+//! Render Phase
+// render(updated data)
+//!Commit Phase
+//<HTML Updated (Updated data)/>
+//componentDidUpdate
