0244c1a20a8d839d91029660fcb49e0f3f12bcc7
ate32 <ate32.champ@gmail.com>
2025-08-06 22:36:17 +0530

    Update coverage reports and enhance tests for Contact and Header components

--- src/components/__tests__/Contact.spec.js
+++ src/components/__tests__/Contact.spec.js
@@ -2,19 +2,29 @@ import { render, screen } from "@testing-library/react";
 import Contact from "../Contact";
 import "@testing-library/jest-dom";
 
-test("Should render Contact component with correct heading text", () => {
-  render(<Contact />);
-  const headingElement = screen.getByText("Contact Us Page 📃");
-  expect(headingElement).toBeInTheDocument();
-});
+describe("Contact Us Component Tests", () => {
+  it("Should render Contact component with correct heading text", () => {
+    render(<Contact />);
+    const headingElement = screen.getByRole("heading");
+    const headingElementText = screen.getByText("Contact Us Page 📃");
+    expect(headingElement).toBeInTheDocument();
+    expect(headingElementText).toBeInTheDocument();
+  });
 
-test("Should render Contact form with input fields and submit button", () => {
-  render(<Contact />);
-  const nameInput = screen.getByPlaceholderText("Your Name");
-  const messageInput = screen.getByPlaceholderText("Your Message");
-  const submitButton = screen.getByRole("button", { name: "Submit" });
+  it("Should render Contact form with input fields and submit button", () => {
+    render(<Contact />);
+    const nameInput = screen.getByPlaceholderText("Your Name");
+    const messageInput = screen.getByPlaceholderText("Your Message");
+    const submitButton = screen.getByRole("button", { name: "Submit" });
 
-  expect(nameInput).toBeInTheDocument();
-  expect(messageInput).toBeInTheDocument();
-  expect(submitButton).toBeInTheDocument();
+    expect(nameInput).toBeInTheDocument();
+    expect(messageInput).toBeInTheDocument();
+    expect(submitButton).toBeInTheDocument();
+  });
+
+  it("Should load 2 input fields in Contact form", () => {
+    render(<Contact />);
+    const inputFields = screen.getAllByRole("textbox");
+    expect(inputFields.length).toBe(2);
+  });
 });
---
+++ src/components/__tests__/Header.spec.js
@@ -0,0 +1,46 @@
+import { fireEvent, screen } from "@testing-library/dom";
+import Header from "../Header";
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import appStore from "../../utils/appStore";
+import { BrowserRouter } from "react-router-dom";
+import "@testing-library/jest-dom";
+
+it("Should render Header component with a login button", () => {
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+  const loginButton = screen.getByRole("button", { name: "Login" });
+  expect(loginButton).toBeInTheDocument();
+});
+
+it("Should render Header component with a cart items", () => {
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+  const cartItems = screen.getByText(/Cart/);
+  expect(cartItems).toBeInTheDocument();
+});
+
+it("Should change my login button to logout button", () => {
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+  const loginButton = screen.getByRole("button", { name: "Login" });
+  fireEvent.click(loginButton);
+
+  const logoutButton = screen.getByRole("button", { name: "Logout" });
+  expect(logoutButton).toBeInTheDocument();
+});
